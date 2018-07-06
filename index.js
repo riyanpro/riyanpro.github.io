31ffe609032078ad8d3e3f516ebee68a1652649c
maimai <764733342@qq.com>
2018-07-06 21:23:07 +0800

    修改端口号

---
+++ index.js
@@ -0,0 +1,19 @@
+//引入http
+let http = require("http");
+//引入fs模块
+let fs = require("fs");
+
+//开启服务
+http
+  .createServer((request, response) => {
+    //设施响应头
+    response.setHeader("content-type", "text/html;chartset=utf-8");
+    //读取文件
+    let temFs = fs.readFileSync("./tem/tem.html", "utf-8");
+    console.log(temFs);
+    //返回
+    response.end(temFs);
+  })
+  .listen(5050, "127.0.0.1", () => {
+    console.log("success");
+  });
